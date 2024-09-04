384f7ac9103bd1b4985e2e5dd8501e2c06506f89
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-09-04 13:56:35 +0530

    View Page Slider Impl

---
+++ app/src/main/java/com/gsl/shoecollection/activities/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.gsl.shoecollection.activities
+
+import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.widget.CompositePageTransformer
+import androidx.viewpager2.widget.MarginPageTransformer
+import com.gsl.shoecollection.adapters.SliderAdapter
+import com.gsl.shoecollection.databinding.ActivityMainBinding
+import com.gsl.shoecollection.model.SliderModel
+import com.gsl.shoecollection.viewModel.MainViewModel
+
+class MainActivity : AppCompatActivity() {
+    private val viewModel = MainViewModel()
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initBanner()
+    }
+
+    private fun initBanner() {
+        binding.progressBarBanner.visibility = View.VISIBLE
+        viewModel.banners.observe(this, Observer { items ->
+            banners(items)
+            binding.progressBarBanner.visibility = View.GONE
+        })
+        viewModel.loadBanners()
+    }
+
+    private fun banners(images: List<SliderModel>) {
+        binding.viewpagerSlider.adapter = SliderAdapter(images, binding.viewpagerSlider)
+        binding.viewpagerSlider.clipToPadding = false
+        binding.viewpagerSlider.clipChildren = false
+        binding.viewpagerSlider.offscreenPageLimit = 3
+        binding.viewpagerSlider.getChildAt(0).overScrollMode = RecyclerView.OVER_SCROLL_NEVER
+
+        val compositePageTransformer = CompositePageTransformer().apply {
+            addTransformer(MarginPageTransformer(40))
+        }
+        binding.viewpagerSlider.setPageTransformer(compositePageTransformer)
+        if (images.size > 1) {
+            binding.dotsIndicator.visibility = View.VISIBLE
+            binding.dotsIndicator.attachTo(binding.viewpagerSlider)
+        }
+    }
+}
