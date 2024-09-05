c7b0c766decd86373d200411e5f38d05974a30e0
Gyarsilal Solanki <gyarsilalsolanki011@gmail.com>
2024-09-05 18:58:24 +0530

    Detail Activity Designed

---
+++ app/src/main/java/com/gsl/shoecollection/activities/DetailActivity.kt
@@ -0,0 +1,13 @@
+package com.gsl.shoecollection.activities
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.gsl.shoecollection.R
+
+class DetailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+
+    }
+}
